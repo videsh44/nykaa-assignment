86028643105885583ba2c8e4c430680826267956
videsh <videsh.ghodarop@iaugmentor.com>
2020-10-08 20:01:22 +0530

    Updates

---
+++ precache-manifest.4b5768761fdb875eb79adb8c88e421f9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f196653cbd25275ca803849a2f6a6ce7",
+    "url": "/nykaa-assignment/index.html"
+  },
+  {
+    "revision": "8a97c6a6cefc27888a86",
+    "url": "/nykaa-assignment/static/css/2.99ee2f66.chunk.css"
+  },
+  {
+    "revision": "d86843b7e9b80271f219",
+    "url": "/nykaa-assignment/static/css/main.9e027bd5.chunk.css"
+  },
+  {
+    "revision": "8a97c6a6cefc27888a86",
+    "url": "/nykaa-assignment/static/js/2.fe2dcce9.chunk.js"
+  },
+  {
+    "revision": "3864c2473b354c6f46ccaaed64e62b51",
+    "url": "/nykaa-assignment/static/js/2.fe2dcce9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d86843b7e9b80271f219",
+    "url": "/nykaa-assignment/static/js/main.1839cca2.chunk.js"
+  },
+  {
+    "revision": "238eb246e269110cdabc",
+    "url": "/nykaa-assignment/static/js/runtime-main.3fd1e79d.js"
+  },
+  {
+    "revision": "7ed3f404ab7a1c00dc470f0b9616bfaf",
+    "url": "/nykaa-assignment/static/media/nykaa_logo.7ed3f404.svg"
+  }
+]);
